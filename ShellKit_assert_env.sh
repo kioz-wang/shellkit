bcea3c3563391ce4662f426dcbfd93b38db6f937
Kioz Wang <never.had@outlook.com>
2024-06-16 04:33:03 +0800

    [特性新增][assert_env] 支持命令路径自检测,优化日志

--- ShellKit_assert_env.sh
+++ ShellKit_assert_env.sh
@@ -19,22 +19,46 @@ function ShellKit_assert_env() {
     local ename="${!einfo:0:1}"
     local epath="${!einfo:1:1}"
     local echeck=("${!einfo:2}")
+    local epath_base=${epath##*/}
 
-    ename="${ename@U}"
-    # echo "einfo name(${ename}) path(${epath}) check(${echeck[*]})"
+    # shellcheck disable=SC2034
+    local -r STY_VERB="\e[2m"
+    # shellcheck disable=SC2034
+    local -r STY_INFO="\e[92m"
+    local -r STY_WARN="\e[48;2;255;255;0m"
+    local -r STY_ERRO="\e[41m"
+    local -r STY_REST="\e[0m"
+
+    # echo -e "${STY_VERB}einfo(${ename}) Assertting path(${epath}),check(${echeck[*]})${STY_REST}"
     if [[ -v "${ename}" ]]; then
-        # echo "einfo Already (${ename}=${!ename})"
+        # echo -e "${STY_VERB}einfo(${ename}) Already path(${!ename})${STY_REST}"
         # shellcheck disable=SC2163
         export "${ename}"
         return
     fi
+    if [[ -z ${epath_base} ]]; then
+        echo -e "${STY_ERRO}FailAssertEnv(${ename}) Invalid path(${epath})${STY_REST}"
+        exit 1
+    fi
+    if [[ ${epath} == "${epath_base}" ]]; then
+        epath=$(command -v "${epath_base}")
+        if [[ -z ${epath} ]]; then
+            echo -e "${STY_ERRO}FailAssertEnv(${ename}) Detect(${epath_base}) notfound${STY_REST}"
+            exit 1
+        fi
+        if [[ ${epath} = alias* ]]; then
+            echo -e "${STY_ERRO}FailAssertEnv(${ename}) Detect(${epath_base}) alias found in path(${epath}), should absolute${STY_REST}"
+            exit 1
+        fi
+        # echo -e "${STY_INFO}einfo(${ename}) Detect(${epath_base}) path(${epath})${STY_REST}"
+    fi
     export "${ename}"="${epath}"
     if ((${#echeck[@]} == 0)) ; then
-        # echo "einfo Force (${ename}=${epath})"
+        # echo -e "${STY_WARN}einfo(${ename}) Check(Skip) force to path(${epath})${STY_REST}"
         return
     fi
     if ! ${epath} "${echeck[@]}" &> /dev/null; then
-        echo "FailAssertEnv (${ename}=${epath}) ${echeck[*]}"
+        echo -e "${STY_ERRO}FailAssertEnv(${ename}) Check(${echeck[*]}) fail at path(${epath})${STY_REST}"
         exit 1
     fi
 }
@@ -53,28 +77,28 @@ fi
 declare -a ShellKit_ENV_lst=()
 
 # shellcheck disable=SC2034
-declare -a ShellKit_ENV_ECHO=(ECHO /usr/bin/echo --version)
+declare -a ShellKit_ENV_ECHO=(ECHO echo)
 ShellKit_ENV_lst+=(ShellKit_ENV_ECHO[@])
 # shellcheck disable=SC2034
-declare -a ShellKit_ENV_CAT=(CAT /usr/bin/cat --version)
+declare -a ShellKit_ENV_CAT=(CAT cat)
 ShellKit_ENV_lst+=(ShellKit_ENV_CAT[@])
 # shellcheck disable=SC2034
-declare -a ShellKit_ENV_SLEEP=(SLEEP /usr/bin/sleep --version)
+declare -a ShellKit_ENV_SLEEP=(SLEEP sleep)
 ShellKit_ENV_lst+=(ShellKit_ENV_SLEEP[@])
 # shellcheck disable=SC2034
-declare -a ShellKit_ENV_OPENSSL=(OPENSSL /usr/bin/openssl version)
+declare -a ShellKit_ENV_OPENSSL=(OPENSSL openssl version)
 ShellKit_ENV_lst+=(ShellKit_ENV_OPENSSL[@])
 # shellcheck disable=SC2034
-declare -a ShellKit_ENV_RM=(RM /usr/bin/rm --version)
+declare -a ShellKit_ENV_RM=(RM rm)
 ShellKit_ENV_lst+=(ShellKit_ENV_RM[@])
 # shellcheck disable=SC2034
-declare -a ShellKit_ENV_WC=(WC /usr/bin/wc --version)
+declare -a ShellKit_ENV_WC=(WC wc --version)
 ShellKit_ENV_lst+=(ShellKit_ENV_WC[@])
 # shellcheck disable=SC2034
-declare -a ShellKit_ENV_LS=(LS /usr/bin/ls --version)
+declare -a ShellKit_ENV_LS=(LS /usr/bin/ls)
 ShellKit_ENV_lst+=(ShellKit_ENV_LS[@])
 # shellcheck disable=SC2034
-declare -a ShellKit_ENV_MV=(MV /usr/bin/mv --version)
+declare -a ShellKit_ENV_MV=(MV mv)
 ShellKit_ENV_lst+=(ShellKit_ENV_MV[@])
 # shellcheck disable=SC2034
 declare -a ShellKit_ENV_GREP=(GREP /usr/bin/grep --version)
